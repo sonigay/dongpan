65771d7659d0ec4cb964cc1521b9486a582f2ea0
sonigay <43840048+sonigay@users.noreply.github.com>
2020-03-05 01:08:07 +0900

    Update test.py

--- test.py
+++ test.py
@@ -523,33 +523,35 @@ async def on_message(message):
 			await client.send_message(message.channel, embed=discord.Embed(description='타이머 종료'))
 			
 			
-			
-options1 = Options()
-options1.add_argument('--start-fullscreen')
-options1.headless = True
-driver = webdriver.chrome()
-driver.get('https://docs.google.com/spreadsheets/d/1gGOqkMcSau3lXHnP5_UZfEW1rbJOi5czd3w-22QX2j4/pubhtml#')
 
-# 전체 페이지의 사이즈를 구하여 브라우저의 창 크기를 확대하고 스크린캡처를 합니다.
-page_width = driver.execute_script('return document.body.parentNode.scrollWidth')
-page_height = driver.execute_script('return document.body.parentNode.scrollHeight')
-driver.set_window_size("1050", "2000")
-png = driver.get_screenshot_as_png()
+	if message.content.startswith('!사진'):			
+		options1 = Options()
+		options1.add_argument('--start-fullscreen')
+		options1.headless = True
+		driver = webdriver.chrome()
+		driver.get('https://docs.google.com/spreadsheets/d/1gGOqkMcSau3lXHnP5_UZfEW1rbJOi5czd3w-22QX2j4/pubhtml#')
 
-# 특정 element의 위치를 구하고 selenium 창을 닫습니다.
-element = driver.find_element_by_class_name('waffle')
-image_location = element.location
-image_size = element.size
-driver.quit()
+		# 전체 페이지의 사이즈를 구하여 브라우저의 창 크기를 확대하고 스크린캡처를 합니다.
+		page_width = driver.execute_script('return document.body.parentNode.scrollWidth')
+		page_height = driver.execute_script('return document.body.parentNode.scrollHeight')
+		driver.set_window_size("1050", "2000")
+		png = driver.get_screenshot_as_png()
 
-# 이미지를 element의 위치에 맞춰서 crop 하고 저장합니다.
-im = Image.open(BytesIO(png))
-left = image_location['x']
-top = image_location['y']
-right = image_location['x'] + image_size['width']
-bottom = image_location['y'] + image_size['height']
-im = im.crop((left, top, right, bottom))
-im.save('./screenshot/정책표.png')
+		# 특정 element의 위치를 구하고 selenium 창을 닫습니다.
+		element = driver.find_element_by_class_name('waffle')
+		image_location = element.location
+		image_size = element.size
+		driver.quit()
+
+		# 이미지를 element의 위치에 맞춰서 crop 하고 저장합니다.
+		im = Image.open(BytesIO(png))
+		left = image_location['x']
+		top = image_location['y']
+		right = image_location['x'] + image_size['width']
+		bottom = image_location['y'] + image_size['height']
+		im = im.crop((left, top, right, bottom))
+		im.save('./screenshot/정책표.png')
+		await client.send_message(message.channel, "사진")
 
 
 
