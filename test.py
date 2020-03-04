26ffdd2115ab15ecb8cf9b44512a56af7941be81
sonigay <43840048+sonigay@users.noreply.github.com>
2020-03-05 00:07:13 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,10 @@ import os
 import datetime
 from time import sleep
 import arrow
+from selenium import webdriver
+from PIL import Image
+from io import BytesIO
+from selenium.webdriver.chrome.options import Options
 from urllib.request import urlopen, Request
 import urllib
 import urllib.request
@@ -517,6 +521,53 @@ async def on_message(message):
 		else:
 			print("땡")
 			await client.send_message(message.channel, embed=discord.Embed(description='타이머 종료'))
+			
+			
+			
+options = Options()
+options.add_argument('--start-fullscreen')
+options.headless = True
+driver = webdriver.Chrome(executable_path='./chromedriver', chrome_options=options)
+driver.get('https://docs.google.com/spreadsheets/d/1gGOqkMcSau3lXHnP5_UZfEW1rbJOi5czd3w-22QX2j4/pubhtml#')
+
+# 전체 페이지의 사이즈를 구하여 브라우저의 창 크기를 확대하고 스크린캡처를 합니다.
+page_width = driver.execute_script('return document.body.parentNode.scrollWidth')
+page_height = driver.execute_script('return document.body.parentNode.scrollHeight')
+driver.set_window_size("1050", "2000")
+png = driver.get_screenshot_as_png()
+
+# 특정 element의 위치를 구하고 selenium 창을 닫습니다.
+element = driver.find_element_by_class_name('waffle')
+image_location = element.location
+image_size = element.size
+driver.quit()
+
+# 이미지를 element의 위치에 맞춰서 crop 하고 저장합니다.
+im = Image.open(BytesIO(png))
+left = image_location['x']
+top = image_location['y']
+right = image_location['x'] + image_size['width']
+bottom = image_location['y'] + image_size['height']
+im = im.crop((left, top, right, bottom))
+im.save('./screenshot/정책표.png')
+
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!고양이'):
+        embed = discord.Embed(
+            title='고양이는',
+            description='멍멍',
+            colour=discord.Colour.green()
+        )
+
+        urlBase = 'https://loremflickr.com/320/240?lock='
+        randomNum = random.randrange(1, 30977)
+        urlF = urlBase+str(randomNum)
+        embed.set_image(url = urlF)
+        await client.send_message(message.channel, embed=embed)
+			
 		
 		
 		
