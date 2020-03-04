3659b6d2f7bc3b4c74a7effa855210bc63bef64e
sonigay <43840048+sonigay@users.noreply.github.com>
2020-03-05 01:54:53 +0900

    Update test.py

--- test.py
+++ test.py
@@ -561,7 +561,7 @@ async def on_message(message):
 # 		urlBase = './screenshot/정책표.png'
 # 		urlF = urlBase
 # 		img = message.set_image(url = urlF)
-		await client.send_message(message.channel, file='./screenshot/정책표.png')
+		await client.send_message(message.channel, './screenshot/정책표.png')
 			
 		
 		
