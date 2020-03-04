0e95d155b60b9677d93e63b14ebb4782cad44358
sonigay <43840048+sonigay@users.noreply.github.com>
2020-03-05 03:25:16 +0900

    Update test.py

--- test.py
+++ test.py
@@ -526,9 +526,10 @@ async def on_message(message):
 
 	if message.content.startswith('!사진'):			
 		options = Options()
+		options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
 		options.add_argument('--start-fullscreen')
 		options.headless = True
-		driver = webdriver.Chrome(executable_path='/app/.chromedriver/bin/chromedriver', chrome_options=options)
+		driver = webdriver.Chrome(executable_path=str(os.environ.get('CHROMEDRIVER_PATH')), chrome_options=options)
 		driver.get('https://docs.google.com/spreadsheets/d/1gGOqkMcSau3lXHnP5_UZfEW1rbJOi5czd3w-22QX2j4/pubhtml#')
 		# 전체 페이지의 사이즈를 구하여 브라우저의 창 크기를 확대하고 스크린캡처를 합니다.
 		page_width = driver.execute_script('return document.body.parentNode.scrollWidth')
