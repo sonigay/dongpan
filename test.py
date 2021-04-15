d82235cba08ebb518cbc0cc3c6fbad583b8d1451
sonigay <43840048+sonigay@users.noreply.github.com>
2021-04-16 04:40:01 +0900

    Update test.py

--- test.py
+++ test.py
@@ -42,11 +42,20 @@ async def on_ready():
 	print("----------------")
 	await client.change_presence(status=discord.Status.dnd, activity=discord.Game(name="업무지원", type=1), afk=False)
 
+	
+	
+	
 @client.event
 async def on_member_join(member):
-    sleep(1)
-    channel = client.get_channel(661832869521391646)
-    await channel.send('```{1.name} 에 오신것을 환영합니다.\n{0.mention} 님!! \n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다.```')	
+	await member.guild.get_channel(661832869521391646).send(member.mention + '```님 환영합니다. \n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다.```')
+	return	
+	
+	
+#@client.event
+#async def on_member_join(member):
+#    sleep(1)
+#    channel = client.get_channel(661832869521391646)
+#    await channel.send('```{1.name} 에 오신것을 환영합니다.\n{0.mention} 님!! \n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다.```')	
 	
 #@client.event
 #async def on_member_join(member):
