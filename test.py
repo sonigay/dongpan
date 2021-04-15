4c4b79ecd5e2c6d172393ffa950c0f86bcac1e1f
sonigay <43840048+sonigay@users.noreply.github.com>
2021-04-16 03:08:53 +0900

    Update test.py

--- test.py
+++ test.py
@@ -41,13 +41,21 @@ async def on_ready():
 	print("----------------")
 	await client.change_presence(status=discord.Status.dnd, activity=discord.Game(name="업무지원", type=1), afk=False)
 
+#@client.event
+#async def on_member_join(member):
+#    sleep(1)	
+#    channel = client.get_channel(661832869521391646)
+    
+#    await message.member.send('```방갑습니다.```')
+#    await message.channel.send('```{1.name} 에 오신것을 환영합니다.\n{0.mention} 님!! \n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다.```')
+
 @client.event
 async def on_member_join(member):
-    sleep(1)	
-    channel = client.get_channel(661832869521391646)
-    
-    await message.member.send('```방갑습니다.```')
-    await message.channel.send('```{1.name} 에 오신것을 환영합니다.\n{0.mention} 님!! \n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다.```')
+    fmt = '''
+    {1.name}에 오신걸 환영합니다! {0.mention}님!
+\n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다.'''
+    channel = member.server.get_channel(661832869521391646)
+    await client.send_message(channel, fmt.format(member, member.server))
 
 @client.event
 async def on_message(message):
