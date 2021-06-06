e5ff1421b2df7e01ad5644818de174aa3f39397f
sonigay <43840048+sonigay@users.noreply.github.com>
2021-06-07 03:12:26 +0900

    Update test.py

--- test.py
+++ test.py
@@ -73,19 +73,19 @@ async def on_ready():
 #    await message.member.send('```방갑습니다.```')
 #    await message.channel.send('```{1.name} 에 오신것을 환영합니다.\n{0.mention} 님!! \n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다.```')
 
-#@client.event
-#async def on_member_join(member):
-#    sleep(1)
-#    fmt = '```{1.name}에 오신걸 환영합니다! {0.mention}님!\n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다.```'
-#    channel = member.server.get_channel(661832869521391646)
-#    await client.send_message(channel, fmt.format(member, member.server))
-
 @client.event
 async def on_member_join(member):
 	sleep(1)
-	fmt = '```{1.name}에 오신걸 환영합니다! {0.mention}님!\n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다.```'.format(member, member.server)
+	fmt = '```{1.name}에 오신걸 환영합니다! {0.mention}님!\n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다.```'
 	channel = member.server.get_channel(661832869521391646)
-	await client.get_channel(channel).send(fmt)
+	await client.send_message(channel, fmt.format(member, member.server))
+
+#@client.event
+#async def on_member_join(member):
+#	sleep(1)
+#	fmt = '```{1.name}에 오신걸 환영합니다! {0.mention}님!\n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다.```'.format(member, member.server)
+#	channel = member.server.get_channel(661832869521391646)
+#	await client.get_channel(channel).send(fmt)
 
 
 @client.event
