e1fbdeaf069f599d49c50b21e911b330f4a5676b
sonigay <43840048+sonigay@users.noreply.github.com>
2021-07-22 17:21:12 +0900

    Update test.py

--- test.py
+++ test.py
@@ -44,64 +44,6 @@ async def on_ready():
 
 
 	
-#@client.event
-#async def on_member_join(member):
-#	sleep(1)
-#	msg = '```<@{}>님 환영합니다. \n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다.```'.format(str(member.id))
-#	await client.get_channel(661832869521391646).send(msg)
-#	return None
-
-	
-	
-#@client.event
-#async def on_member_join(member):
-#	await client.get_channel(661832869521391646).send(member.mention + '```님 환영합니다. \n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다.```')
-#	return	
-	
-	
-#@client.event
-#async def on_member_join(member):
-#    sleep(1)
-#    channel = client.get_channel(661832869521391646)
-#    await channel.send('```{1.name} 에 오신것을 환영합니다.\n{0.mention} 님!! \n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다.```')	
-	
-#@client.event
-#async def on_member_join(member):
-#    sleep(1)	
-#    channel = client.get_channel(661832869521391646)
-    
-#    await message.member.send('```방갑습니다.```')
-#    await message.channel.send('```{1.name} 에 오신것을 환영합니다.\n{0.mention} 님!! \n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다.```')
-
-#@client.event
-#async def on_member_join(member):
-#	sleep(1)
-#	fmt = '```{1.name}에 오신걸 환영합니다! {0.mention}님!\n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다.```'.format(member, member.server)
-#	await client.get_channel(661832869521391646).send(fmt)
-
-#@client.event
-#async def on_member_join(self, member):
-#	sleep(1)
-#	fmt = '```{1.name}에 오신걸 환영합니다! {0.mention}님!\n거래처권한요청채널에\n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다.```'
-
-#	await self.client.get_channel(661832869521391646).send(fmt)
-
-
-
-	
-#@client.event
-#async def on_member_join(member):
-#	sleep(1)
-#	fmt = '```{1.name}에 오신걸 환영합니다! {0.mention}님!\n매장이름/직급/성함/연락처 이렇게 남겨주시면 \n확인후 권한을 승인해드리겠습니다.```'.format(member, member.server)
-#	channel = member.server.get_channel(661832869521391646)
-#	await client.get_channel(channel).send(fmt)
-@client.event
-async def on_member_join(member):
-
-	await on_member_join.send(f'member has joined the server.')
-
-
-
 
 
 @client.event
